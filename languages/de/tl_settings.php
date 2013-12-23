924152f6accd6145c3aabd1b44467acabcd9415c
Glen Langer <contao.modul@glen-langer.de>
2013-12-23 22:15:44 +0100

    Fixed #16, Cron Label identisch zum Core Cron Label

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -18,7 +18,7 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_settings']['cron_legend']     = "CRON Erweiterung";
+$GLOBALS['TL_LANG']['tl_settings']['cron_scheduler_legend']     = "CRON/Scheduler Erweiterung";
 $GLOBALS['TL_LANG']['tl_settings']['cron_limit']['0'] = "Zeitbegrenzung";
 $GLOBALS['TL_LANG']['tl_settings']['cron_limit']['1'] = "Geben Sie die Zeitbegrenzung in Sekunden ein, nach der jeder Job automatisch unterbrochen wird, sofern der Job dieses Feature unterstützt. (0 deaktiviert alle Cron-Jobs).";
  
