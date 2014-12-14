55d76c2f515c2b800f6ff28750fbb8154b9672d5
Glen Langer <contao.modul@glen-langer.de>
2014-12-14 21:56:05 +0100

    Fixed #20, Anpassung initialize.php Aufruf

--- jobs/PurgeLog.php
+++ jobs/PurgeLog.php
@@ -19,7 +19,20 @@
  * Initialize the system
  */
 define('TL_MODE', 'BE');
-require_once('../../../initialize.php');
+
+$dir = __DIR__;
+
+while ($dir != '.' && $dir != '/' && !is_file($dir . '/system/initialize.php'))
+{
+    $dir = dirname($dir);
+}
+
+if (!is_file($dir . '/system/initialize.php'))
+{
+    echo 'Could not find initialize.php!';
+    exit(1);
+}
+require($dir . '/system/initialize.php');
 
 /**
  * Class PurgeLog
